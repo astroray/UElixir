ae23e63568f88b061c3a7f0e648d7407d7f1e512
INYEOL CHOI <korea073@gmail.com>
2019-10-01 08:55:07 +0900

    WorkerThread Attribute

---
+++ Assets/UElixir/Attributes/WorkerThreadAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UElixir
+{
+    /// <summary>
+    /// Marks the method will be called by worker thread.
+    /// Target method will be called from worker thread. It should be push the task calling Unity API to the main thread queue.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public sealed class WorkerThreadAttribute : Attribute
+    {
+    }
+}
